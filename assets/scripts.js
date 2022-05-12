2a9bd9715339b15a59f0e975d813b95efeb7a332
Yuchan <yu.nakasone@gmail.com>
2022-05-12 18:08:52 -0300

    termine el aside menu :)

--- assets/scripts.js
+++ assets/scripts.js
@@ -34,20 +34,26 @@ document.getElementById("social-fb").onclick = function () {
 
 
 // Hamburger Menu interactions
+const labels = document.querySelectorAll('.aside-menu nav a');
+console.log(labels);
 let menuBtn = document.getElementById("ham-menu");
 let hamInner = document.querySelector('.ham-inner');
+let aside = document.querySelector('.aside-menu');
 let menuOpen = false;
+
 
 function blurBackground(flag){
 // Recibe como parametro un booleano. Si es true, blurea el fondo. Si es false, lo desblurea.
     let title = document.querySelector('.overlay-info');
     let main = document.querySelector('.main');
+    let blur = 'blur(3px)';
+    let unblur = 'blur(0px)';
     if (flag == true) {
-        title.style.filter = 'blur(2px)';
-        main.style.filter = 'blur(2px)';
+        title.style.filter = blur;
+        main.style.filter = blur;
     } else {
-        title.style.filter = 'blur(0px)';
-        main.style.filter = 'blur(0px)';
+        title.style.filter = unblur;
+        main.style.filter = unblur;
     }
 }
 
@@ -55,11 +61,24 @@ function blurBackground(flag){
 menuBtn.addEventListener('click', function() {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        aside.classList.add('active');
         blurBackground(true);
         menuOpen = true;
     }else {
         menuBtn.classList.remove('open');
+        aside.classList.remove('active');
         blurBackground(false);
         menuOpen = false;
     }
+});
+
+
+
+labels.forEach(function (label) {
+    // Recide un array de labels y les agrega el evento onClick, el cual esconde el menuAside.
+    label.addEventListener('click', () => {
+        menuBtn.classList.remove('open');
+        aside.classList.remove('active');
+        blurBackground(false);
+    });
 });
